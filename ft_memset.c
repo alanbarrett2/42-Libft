1c12c05242cb343dc2264d4a3afdf8204a542103
alanbarrett2 <41269411+alanbarrett2@users.noreply.github.com>
2018-11-12 21:01:53 -0800

    Create ft_memset.c
    How to write the memset function in c programing. 42 cadet Silicon Valley. Alan Roger Barrett II.

---
+++ ft_memset.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memset.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albarret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/04 22:30:25 by albarret          #+#    #+#             */
+/*   Updated: 2018/11/12 20:53:49 by albarret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*ptr;
+
+	ptr = (unsigned char*)b;
+	while (len-- > 0)
+		*(ptr++) = (unsigned char)c;
+	return (b);
+}
