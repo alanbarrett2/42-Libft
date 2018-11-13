bd569dabb90614a32842f55a28b7a09f0bb78be8
alanbarrett2 <41269411+alanbarrett2@users.noreply.github.com>
2018-11-12 21:05:50 -0800

    Create ft_memcpy.c
    How to write the memcpy function in c programing. 42 cadet Silicon Valley. Alan Roger Barrett II.

---
+++ ft_memcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albarret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/05 11:52:33 by albarret          #+#    #+#             */
+/*   Updated: 2018/11/09 20:31:22 by albarret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char		*ptr;
+	const unsigned char	*ptr2;
+
+	ptr = (unsigned char*)dst;
+	ptr2 = (unsigned char*)src;
+	while (n-- > 0)
+		*(ptr++) = *(ptr2++);
+	return (dst);
+}
