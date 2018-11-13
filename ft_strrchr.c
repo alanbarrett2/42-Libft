c604eadbbb168324645a1ac88ae607864f38c7fa
alanbarrett2 <41269411+alanbarrett2@users.noreply.github.com>
2018-11-12 21:30:03 -0800

    Create ft_strrchr.c
    How to write the strrchr function in c programing. 42 libft.h cadet Silicon Valley. Alan Roger Barrett II.

---
+++ ft_strrchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albarret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/05 15:13:43 by albarret          #+#    #+#             */
+/*   Updated: 2018/11/12 21:29:28 by albarret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	char	*last_pos;
+
+	last_pos = (0);
+	while (*s)
+	{
+		if (*s == c)
+			last_pos = (char*)s;
+		++s;
+	}
+	if (last_pos)
+		return (last_pos);
+	if (c == '\0')
+		return ((char*)s);
+	return (0);
+}
