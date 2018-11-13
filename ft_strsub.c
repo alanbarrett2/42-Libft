b0113f89c6eae60740cd18051e2fb508634bc6be
alanbarrett2 <41269411+alanbarrett2@users.noreply.github.com>
2018-11-12 22:02:54 -0800

    Create ft_strsub.c
    How to write the strsub function in c programing. 42 libft.h cadet Silicon Valley. Alan Roger Barrett II.

---
+++ ft_strsub.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albarret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/06 13:59:22 by albarret          #+#    #+#             */
+/*   Updated: 2018/11/12 22:02:20 by albarret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char			*result;
+
+	if (!s || !(result = ft_memalloc(len + 1)))
+		return (NULL);
+	result = ft_strncpy(result, (char*)s + start, len);
+	return (result);
+}
