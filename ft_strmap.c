dddc4ed4cd7d1a51ab860f1839d97ef6caef00d3
alanbarrett2 <41269411+alanbarrett2@users.noreply.github.com>
2018-11-12 21:57:08 -0800

    Create ft_strmap.c
    How to write the strmap function in c programing. 42 libft.h cadet Silicon Valley. Alan Roger Barrett II.

---
+++ ft_strmap.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albarret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/06 12:09:45 by albarret          #+#    #+#             */
+/*   Updated: 2018/11/12 21:56:36 by albarret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	char	*ptr;
+	char	*ptr2;
+	char	*result;
+
+	if (!s || !(result = ft_memalloc((size_t)ft_strlen((char*)s) + 1)))
+		return (NULL);
+	ptr = (char*)s;
+	ptr2 = result;
+	while (*ptr)
+		*(ptr2++) = f(*(ptr++));
+	return (result);
+}
