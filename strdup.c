ed5bff6a1826c917e49183395041cff26e1f1ec4
alanbarrett2 <41269411+alanbarrett2@users.noreply.github.com>
2018-11-12 21:19:02 -0800

    Create strdup.c
    How to write the strdup function in c programing. 42 libft.h cadet Silicon Valley. Alan Roger Barrett II.

---
+++ strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albarret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/12 21:18:36 by albarret          #+#    #+#             */
+/*   Updated: 2018/11/12 21:18:42 by albarret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *src)
+{
+	char	*new;
+	int		i;
+	int		size;
+
+	size = 0;
+	while (src[size])
+		size++;
+	if (!(new = malloc(sizeof(char) * (size + 1))))
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		new[i] = src[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
