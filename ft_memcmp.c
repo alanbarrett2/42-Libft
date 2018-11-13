54c46fe017b6173b10bb590d9229a627a995d4cb
alanbarrett2 <41269411+alanbarrett2@users.noreply.github.com>
2018-11-12 21:15:13 -0800

    Create ft_memcmp.c
    How to write the memcmp function in c programing. 42 libft.h cadet Silicon Valley. Alan Roger Barrett II.

---
+++ ft_memcmp.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albarret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/09 20:36:35 by albarret          #+#    #+#             */
+/*   Updated: 2018/11/12 21:14:32 by albarret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*ptr;
+	unsigned char	*ptr2;
+
+	if (n == 0)
+		return (0);
+	ptr = (unsigned char*)s1;
+	ptr2 = (unsigned char*)s2;
+	i = 0;
+	while (*ptr == *ptr2 && ++i < n)
+	{
+		ptr++;
+		ptr2++;
+	}
+	return ((int)(*ptr - *ptr2));
+}
