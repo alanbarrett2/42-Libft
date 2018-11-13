477c33ec0fe7a1e5fb4102b40d6d4b54d6e101ad
alanbarrett2 <41269411+alanbarrett2@users.noreply.github.com>
2018-11-12 22:15:31 -0800

    Create ft_lstnew.c
    How to write the lstnew function in c programing. 42 libft.h cadet Silicon Valley. Alan Roger Barrett II.

---
+++ ft_lstnew.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albarret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/06 19:31:14 by albarret          #+#    #+#             */
+/*   Updated: 2018/11/09 20:26:00 by albarret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void	const *content, size_t content_size)
+{
+	t_list	*list;
+
+	if (!(list = (t_list*)ft_memalloc(sizeof(t_list))))
+		return (NULL);
+	if (!content)
+	{
+		list->content = NULL;
+		list->content_size = 0;
+	}
+	else
+	{
+		if ((list->content = ft_dup(content, content_size)) == NULL)
+			return (NULL);
+		list->content_size = content_size;
+	}
+	list->next = NULL;
+	return (list);
+}
