480a488d6121443d4423ff3cfe987c73402e0ef7
alanbarrett2 <41269411+alanbarrett2@users.noreply.github.com>
2018-11-12 22:17:30 -0800

    Create ft_lstdel.c
    How to write the lstdel function in c programing. 42 libft.h cadet Silicon Valley. Alan Roger Barrett II.

---
+++ ft_lstdel.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albarret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/06 23:40:48 by albarret          #+#    #+#             */
+/*   Updated: 2018/11/12 22:16:57 by albarret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list	*ptr;
+	t_list	*tmp;
+
+	ptr = *alst;
+	while (ptr)
+	{
+		tmp = ptr;
+		ft_lstdelone(&ptr, del);
+		ptr = tmp->next;
+	}
+	*alst = NULL;
+}
