6db2321208ad593c34a34e01c3e46446a27f5765
alanbarrett2 <41269411+alanbarrett2@users.noreply.github.com>
2018-11-12 22:14:43 -0800

    Create ft_putnbr_fd.c
    How to write the putnbr_fd function in c programing. 42 libft.h cadet Silicon Valley. Alan Roger Barrett II.

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albarret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/06 15:39:34 by albarret          #+#    #+#             */
+/*   Updated: 2018/11/12 22:14:10 by albarret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int nb, int fd)
+{
+	unsigned int	nbr;
+
+	if (nb < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nbr = (unsigned int)(nb * -1);
+	}
+	else
+		nbr = (unsigned int)nb;
+	if (nbr >= 10)
+		ft_putnbr_fd(nbr / 10, fd);
+	ft_putchar_fd((char)(nbr % 10 + 48), fd);
+}
