2f6a18d2e793131a06543882241aaadc6e4f53bc
Alan Roger Barrett II <41269411+alanbarrett2@users.noreply.github.com>
2018-11-12 23:35:04 -0800

    Create ft_strtrim.c
    Unfinished at this moment.

---
+++ ft_strtrim.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albarret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/12 17:28:40 by albarret          #+#    #+#             */
+/*   Updated: 2018/11/12 23:34:20 by albarret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static	int		ft_len_no_esc(int start, int end)
+{
+
+	while (start < end)
+		start++;
+	return (start);
+}
+
+static int		start(char const *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+		i++;
+	return (i);
+}
+
+static int		end(char const *s)
+{
+	int i;
+	int slen;
+
+	i = 0;
+	slen = (ft_strlen(s));
+	slen = slen - 1;
+	while (s[slen] == ' ' || s[slen] == '\t' || s[slen] == '\n')
+		slen--;
+	return (slen);
+}
+
+char	*ft_strtrim(char const *s)
+{
+	int		start_len;
+	int		end_len;
+	char	*mem;
+
+	start_len = start(s);
+	end_len = end(s);
+	if (!(s || !(mem = malloc(sizeof(char) * (ft_len_no_esc(start_len, end_len)) + 1))))
+		return (NULL);
+	while (start_len < end_len)
+		mem = s[start_len++];
+	return (mem);
+}
+
+int		main(void)
+{
+	char const	*ptr;
+	char		*mem;
+
+	ptr = "' ' '\t' '\n'this'\t'is' 'my'\n'string'\n' '\t'' '";
+	ft_strtrim(ptr);
+	mem = ft_strtrim(ptr);
+	printf("%s\n", mem);
+	return (0);
+}
