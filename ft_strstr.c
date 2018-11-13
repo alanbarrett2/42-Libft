f07a3a891541d432afb1287006a932b3bb29e724
alanbarrett2 <41269411+alanbarrett2@users.noreply.github.com>
2018-11-12 21:31:16 -0800

    Create ft_strstr.c
    How to write the strstr function in c programing. 42 libft.h cadet Silicon Valley. Alan Roger Barrett II.

---
+++ ft_strstr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albarret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/05 15:20:35 by albarret          #+#    #+#             */
+/*   Updated: 2018/11/12 21:30:36 by albarret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(const char *str, const char *to_find)
+{
+	unsigned int pos;
+	unsigned int i;
+
+	if (!*to_find)
+		return ((char*)str);
+	pos = 0;
+	while (str[pos] != '\0')
+	{
+		if (str[pos] == to_find[0])
+		{
+			i = 1;
+			while (to_find[i] != '\0' && str[pos + i] == to_find[i])
+				++i;
+			if (to_find[i] == '\0')
+				return ((char*)&str[pos]);
+		}
+		++pos;
+	}
+	return (0);
+}
