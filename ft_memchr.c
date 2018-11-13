3681ee7fb089d102cbadc840608ec0e5cf2d8377
alanbarrett2 <41269411+alanbarrett2@users.noreply.github.com>
2018-11-12 21:14:08 -0800

    Create ft_memchr.c
    How to write the memchr  function in c programing. 42 libft.h cadet Silicon Valley. Alan Roger Barrett II.

---
+++ ft_memchr.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albarret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/05 12:17:52 by albarret          #+#    #+#             */
+/*   Updated: 2018/11/12 21:13:19 by albarret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	i = 0;
+	ptr = (unsigned char*)s;
+	while (++i <= n)
+		if (*(ptr++) == (unsigned char)c)
+			return ((void*)--ptr);
+	return (0);
+}
