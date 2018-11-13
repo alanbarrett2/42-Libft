fd66587acc32816ffaebd5e0a82cd7aa3a42101b
alanbarrett2 <41269411+alanbarrett2@users.noreply.github.com>
2018-11-12 21:11:35 -0800

    Create ft_memmove.c
    How to write the memmove function in c programing. 42 libft.h cadet Silicon Valley. Alan Roger Barrett II.

---
+++ ft_memmove.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albarret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/05 12:14:04 by albarret          #+#    #+#             */
+/*   Updated: 2018/11/12 21:10:09 by albarret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t				i;
+	unsigned char		*ptr;
+	const unsigned char	*ptr2;
+
+	ptr = (unsigned char*)dst;
+	ptr2 = (unsigned char*)src;
+	i = 0;
+	if (ptr2 < ptr)
+		while (++i <= len)
+			ptr[len - i] = ptr2[len - i];
+	else
+		while (len-- > 0)
+			*(ptr++) = *(ptr2++);
+	return (dst);
+}
